bc486ec860fbae646172e068081f10c11446757c
kyimoemin <kyimoemin0@gmail.com>
2025-07-29 13:50:08 +0700

    removed tests

--- tests/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { greeting } from "../src/index";
-
-describe("greeting function", () => {
-  it('should return "hello world"', () => {
-    expect(greeting()).toBe("hello world");
-  });
-});
