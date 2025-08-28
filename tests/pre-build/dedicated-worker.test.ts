9b77c73e03a247653e5c4d075f5f237e826bda11
kyimoemin <kyimoemin0@gmail.com>
2025-08-28 18:26:59 +0700

    update tests

--- tests/pre-build/dedicated-worker.test.ts
+++ tests/pre-build/dedicated-worker.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { DedicatedWorker } from "../../src/dedicated-worker";
+import { sleep } from "../utils";
 
 describe("DedicatedWorker", () => {
   const workerURL = new URL("./dummy-worker.js", import.meta.url);
@@ -27,7 +28,8 @@ describe("DedicatedWorker", () => {
 
   it("should report busy state", async () => {
     const add = dedicatedWorker.func("add");
-    add(1, 2).then(() => {
+    add(1, 2).then(async () => {
+      await sleep(0);
       expect(dedicatedWorker.busy).toBe(false);
     });
     expect(dedicatedWorker.busy).toBe(true);
@@ -35,15 +37,22 @@ describe("DedicatedWorker", () => {
 
   it("should reject on error", async () => {
     const err = dedicatedWorker.func("error");
-    const promise = err(1, 3);
-    expect(promise).rejects.toThrow("fail");
+    try {
+      await err(1, 3);
+    } catch (e) {
+      expect((e as Error).message).toBe("fail");
+    }
   });
 
   it("should clean up calls and reject all on terminated", async () => {
-    const add = dedicatedWorker.func("add");
-    const promise = add(1, 2);
-    dedicatedWorker.terminate();
-    expect(promise).rejects.toThrow("Worker was terminated");
-    expect(dedicatedWorker.busy).toBe(false);
+    try {
+      const add = dedicatedWorker.func("add");
+      const promise = add(1, 2);
+      dedicatedWorker.terminate();
+      expect(dedicatedWorker.busy).toBe(false);
+      await promise;
+    } catch (e) {
+      expect(e.message).toBe("Worker was terminated");
+    }
   });
 });
